8a9f2c44ed39a78d9047830db7442949f57467bd
tomir1s <asimovatomiris4@gmail.com>
2022-04-15 17:49:31 +0500

    initial commit

---
+++ Checklists/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Jafar Asimov on 4/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
