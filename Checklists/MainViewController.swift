28ec615659d5c442e40ba944bb936017bdad4236
tomir1s <asimovatomiris4@gmail.com>
2022-04-18 18:04:57 +0500

    Checklists groups

--- Checklists/MainViewController.swift
+++ Checklists/MainViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class MainViewController: UITableViewController {
 
+    let groups: [ChecklistGroup] = [
+        ChecklistGroup(title: "Birthdays", imageName: "birthdaypic"),
+        ChecklistGroup(title: "Groceries", imageName: "groceriespic"),
+        ChecklistGroup(title: "To Do", imageName: "todopic"),
+        ChecklistGroup(title: "Business stuff", imageName: "pic")
+    ]
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return groups.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let group: ChecklistGroup = groups[indexPath.row]
+        let cell = UITableViewCell()
+        cell.textLabel?.text = group.title
+        return cell
+    }
 }
 
---
+++ Checklists/Models/ChecklistsGroup.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistsGroup.swift
+//  Checklists
+//
+//  Created by Jafar Asimov on 4/18/22.
+//
+
+import Foundation
+
+struct ChecklistGroup {
+    let title: String
+    let imageName: String
+}
